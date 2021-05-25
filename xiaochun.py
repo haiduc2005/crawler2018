bbb45519bc4ece51afaa4a7e4be4ea0c0344ac05
haiduc2005 <haiduc2005@gmail.com>
2021-05-25 18:49:30 +0900

    Update xiaochun.py

--- xiaochun.py
+++ xiaochun.py
@@ -8,14 +8,14 @@ from urllib.request import Request
 import re
 import os
 
-downloadDir = './sns/'  # 下载后存放目录
-if os.path.exists(downloadDir) == False:  # 判断文件夹是否已经存在
-    os.makedirs(downloadDir)  # 创建文件夹
+downloadDir = './sns/'  # ダウンロード場所
+if os.path.exists(downloadDir) == False:  # フォルダ有り無しチェック
+    os.makedirs(downloadDir)  # フォルダ作成
 
 
 class xiaochun():
     
-    # 搜索
+    # 検索
     def search():
         driver = webdriver.Firefox()
         base_url = "https://www.incnjp.com/forum-92-1.html"
@@ -26,18 +26,18 @@ class xiaochun():
         # driver.find_element_by_xpath("//*[@id='password3_LUyc9']").send_keys("pass")
         # driver.find_element_by_xpath("/html/body/div[6]/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/form/div/div[6]/table/tbody/tr/td[1]/button").click()  # login
 
-        a_s = driver.find_elements_by_xpath("/html/body/div[6]/div[4]/div/div/div[4]/div[2]/form/table/tbody[*]/tr/th/a[2]")#获取帖子链接列表所在的标签a
+        a_s = driver.find_elements_by_xpath("/html/body/div[6]/div[4]/div/div/div[4]/div[2]/form/table/tbody[*]/tr/th/a[2]")#文書URLリストのタグa
         # driver.implicitly_wait(20)
-        urls = []  # 保存本页面所有帖子链接列表
+        urls = []  # 文書URLリスト
         for a in a_s:
-            chapter_href = a.get_attribute("href")  # 帖子链接
+            chapter_href = a.get_attribute("href")  # 文書URL
             
             if -1 != chapter_href.rfind('http'):
                 urls.append(chapter_href)           
         print("贴子总数：",len(urls))
         return urls
         
-    # 按帖子链接爬取内容
+    # 文書URLにより文書を取得
     def get_name_content(urls):
 
         chapters = []
@@ -48,7 +48,7 @@ class xiaochun():
             soup = BeautifulSoup(html, "lxml")
             # soup = BeautifulSoup(html, "html5lib")
             title = soup.findAll('h1')[0].text          
-            print("贴子title：" + title)
+            print("文書title：" + title)
             chapters.append(title)
             content = soup.findAll(id=re.compile("postmessage.*"))[0]
             
@@ -62,7 +62,7 @@ class xiaochun():
             xiaochun.save_book(title,chapters)
             chapters = []
     
-    # 清洗文本格式，一次性写入txt文件：关键是调整格式
+    # txtファイルに書き込み
     def save_book(bookName,chapters):
         bookName = bookName.replace("\n","")
         bookname = downloadDir  + bookName + '.txt'       
@@ -70,10 +70,10 @@ class xiaochun():
         for i in chapters:        
             # file.write('\t')
             for ii in i:
-                if ii.startswith('<div'):  # 去掉每章开头多余的<div……></div>
+                if ii.startswith('<div'):  # 不要な<div……></div>を除く
                     ii = ""
                 file.write(ii)
-            # file.write('\n')  #每写完一句，换行，控制文本格式
+            # file.write('\n')  #改行
 
 
 if __name__ == "__main__":
